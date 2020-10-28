8aced08e856df701ffc4ac0fcb151280aae1c792
habibijaelani05 <habibijaelani05@gmail.com>
2020-10-28 20:07:30 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+# DDP LAB-3
+
+# SOAL 1 - Gunting-Batu-Kertas
+# Tuliskan program untuk Soal 1 di bawah ini
+
+
+
+
+
+# SOAL 2 - Toko Buku Bekas
+# Tuliskan program untuk Soal 2 di bawah ini
+
+
+
+
+
+# SOAL 3 - Mencetak Persegi
+# Tuliskan program untuk Soal 3 di bawah ini
+
+
+
+
