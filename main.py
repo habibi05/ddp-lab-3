b3304ff635a48ec0846baae69aa5da1d8476ed08
habibi <habibijaelani05@gmail.com>
2020-11-01 08:31:35 +0000

    add comment

--- main.py
+++ main.py
@@ -2,46 +2,57 @@
 
 # SOAL 1 - Gunting-Batu-Kertas
 # Tuliskan program untuk Soal 1 di bawah ini
+# Simpan masukan banyaknya angka kedalam variabel playerOne dan playerTwo
 playerOne = input("Masukkan pilihan Player-1: ")
 playerTwo = input("Masukkan pilihan Player-2: ")
 
-# Gunting mengalahkan Kertas
-# Kertas mengalahkan Batu
-# Batu mengalahkan Gunting
+# Sistem melakukan pengecekan dengan kondisi jika
+# Player-1 menginputkan Gunting dan Player-2 menginputkan Kertas maka Player-1 menang
+# Player-1 menginputkan Kertas dan Player-2 menginputkan Batu maka Player-1 menang
+# Player-1 menginputkan Batu dan Player-2 menginputkan Gunting maka Player-1 menang
 if (playerOne == "gunting" and playerTwo == "kertas") or (playerOne == "kertas" and playerTwo == "batu") or (playerOne == "batu" and playerTwo == "gunting"):
-  print("\nPlayer-1 Menang.")
+  print("Player-1 Menang.")
 elif (playerTwo == "gunting" and playerOne == "kertas") or (playerTwo == "kertas" and playerOne == "batu") or (playerTwo == "batu" and playerOne == "gunting"):
-  print("\nPlayer-2 Menang.")
+  print("Player-2 Menang.")
 elif playerOne == playerTwo:
-  print("\nPertandingan seri.")
+  print("Pertandingan seri.")
 else:
-  print("\nMasukan Salah")
+  print("Masukan Salah")
 
 
 
 
 # SOAL 2 - Toko Buku Bekas
 # Tuliskan program untuk Soal 2 di bawah ini
+# Simpan masukan banyaknya buku kedalam variabel jumlahBuku
 jumlahBuku = int(input("\nMasukkan banyaknya buku yang akan dibeli: "))
 
+# Jika jumlahBuku lebih dari 50 maka akan dikalikan 10000
 if jumlahBuku > 50:
-  print("\nHarga yang harus dibayar = ", jumlahBuku*10000," rupiah")
+  print("Harga yang harus dibayar = ", jumlahBuku*10000," rupiah")
+# Jika jumlahBuku lebih dari 50 maka akan dikalikan 15000
 elif (jumlahBuku <= 50) and (jumlahBuku >= 26):
-  print("\nHarga yang harus dibayar = ", jumlahBuku*15000," rupiah")
+  print("Harga yang harus dibayar = ", jumlahBuku*15000," rupiah")
+# Jika jumlahBuku lebih dari 50 maka akan dikalikan 18000
 elif (jumlahBuku <= 25) and (jumlahBuku >= 11):
-  print("\nHarga yang harus dibayar = ", jumlahBuku*18000," rupiah")
+  print("Harga yang harus dibayar = ", jumlahBuku*18000," rupiah")
+# Jika jumlahBuku lebih dari 50 maka akan dikalikan 20000
 else:
-  print("\nHarga yang harus dibayar = ", jumlahBuku*20000," rupiah")
+  print("Harga yang harus dibayar = ", jumlahBuku*20000," rupiah")
 
 
 
 
 # SOAL 3 - Mencetak Persegi
 # Tuliskan program untuk Soal 3 di bawah ini
+# Simpan masukan sebuah bilangan bulat kedalam variabel inBilanganBulat
 inBilanganBulat = int(input("\nMasukkan sebuah bilangan bulat: "))
+# Lakukan perulangan sebanyak nilai yang ada di variabel inBilanganBulat
 for valueBilangan in range(inBilanganBulat):
+  # Jika nilai perulangan modulo 2 dan nilainya 0 maka print #
   if valueBilangan % 2 == 0:
     print("# "*inBilanganBulat)
+  # Jika nilai perulangan modulo 2 dan nilainya bukan 0 maka print $
   else:
     print("$ "*inBilanganBulat)
 
